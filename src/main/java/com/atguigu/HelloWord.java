823d714a2811763b85b6c452b2262111568d144c
SongKK <KuokuanSong@163.com>
2020-09-26 17:07:29 +0800

    在IDEA中练习

---
+++ src/main/java/com/atguigu/HelloWord.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author Kuokuan Song
+ * @date 2020/9/26
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("这是一个超级大文件");
+    }
+}
